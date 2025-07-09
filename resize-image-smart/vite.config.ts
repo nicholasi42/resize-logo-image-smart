6d9f0294c62022fdd61be29cb896131da1881739
nicholasi42 <nicholasi@spotify.com>
2025-07-08 18:06:42 -0700

    Update vite.config.ts

--- resize-image-smart/vite.config.ts
+++ resize-image-smart/vite.config.ts
@@ -4,13 +4,12 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // Add this line
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react()
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
